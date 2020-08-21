aa55ed2b49f010861c958a54f58cb0bb96694f23
pmaty <p.matys21@gmail.com>
2020-08-21 10:34:27 +0200

    wyszedl wygaszcz ekranu

--- snake.py
+++ snake.py
@@ -1,11 +1,34 @@
+import os
 tab = []
 line = []
-for i in range(40):
-    for j in range(12):
-        line.append("X")
-    tab.append(line)
-for i in range(40):
-    for j in range(12):
-        print(tab[i][j], end="")
-    print("")
-print("=snakeboard=")
+xtab = [5, 6, 7, 8]
+ytab = [39, 40, 41, 42]
+snake = False
+
+for it in range (43):
+    for i in range(40):
+        for j in range(12):
+            line.append("X")
+        tab.append(line)
+        line = []
+
+    for i in range(40):
+        for j in range(12):
+            for s in range(4):
+                if((i == ytab[s]) and (j == xtab[s])):
+                    print("@", end="")
+                    snake = True
+            if(not snake):
+                print(tab[i][j], end="")
+            snake = False
+        print("")
+
+    for s in range(4):
+        xtab[s] = xtab[s] - 1
+        ytab[s] = ytab[s] - 1
+
+        if(xtab[s] < 0):
+            xtab[s] = 11
+
+    print("=snakeboard=")
+    os.system('CLS')
